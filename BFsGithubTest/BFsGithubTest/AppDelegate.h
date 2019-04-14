433511a1961243efe52fb04e8f28eae4fc22390a
BFAlex <838318741@qq.com>
2019-04-14 23:30:43 +0800

    把Mac的项目代码同步到Github中的项目库中

---
+++ BFsGithubTest/BFsGithubTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BFsGithubTest
+//
+//  Created by BFsAlex on 2019/4/14.
+//  Copyright © 2019 BFsAlex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
